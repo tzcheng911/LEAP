79a1fab3bf21f4a6587023e6a7fd40cc6da67fde
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2023-09-15 11:31:16 -0700

    add cbsb vis

--- vis_CBS.py
+++ vis_CBS.py
@@ -33,7 +33,7 @@ runs = ['_01','_02']
 
 subj = [] 
 for file in os.listdir():
-    if file.startswith('cbs_A'):
+    if file.startswith('cbs_b'):
         subj.append(file)
 
 run = runs[0]
@@ -65,7 +65,7 @@ mne.viz.plot_compare_evokeds(evoked_s, picks=chs, combine="mean", show_sensors="
 epoch = mne.read_epochs(file_in + run + '_epochs_subcortical.fif')
 evoked_s = mne.read_evokeds(file_in + run + '_evoked_substd_cabr.fif')[0]
 evoked_d1 = mne.read_evokeds(file_in + run + '_evoked_dev1_cabr.fif')[0]
-evoked_d2 = mne.read_evokeds(file_in + run + '_evoked_dev2_cabr.fif')[0]
+evoked_d2 = mne.read_evokeds(file_in + run + '_evoked_rrays cannot bdev2_cabr.fif')[0]
 evoked_s.crop(tmin=-0.1, tmax=0.2)
 mne.viz.plot_compare_evokeds(evoked_s, picks="meg", axes="topo") # plot all of them
 
@@ -97,25 +97,25 @@ brain = stc.plot(
     mode='glass_brain'
 )
 
-#%%######################################## visualize the group level source average
+#%%######################################## visualize the group level source average for ADULTS
 # load the data
 stc1 = mne.read_source_estimate(root_path + 'cbs_A101/sss_fif/cbs_A101_mmr2_morph-vl.stc')
 stc2 = mne.read_source_estimate(root_path + 'cbs_A101/sss_fif/cbs_A101_mmr2_morph-vl.stc')
-MEG_mmr1_v = np.load(root_path + 'meeg_mmr_analysis/group_mmr1_vector_morph.npy') # with the mag or vector method
-MEG_mmr2_v = np.load(root_path + 'meeg_mmr_analysis/group_mmr2_vector_morph.npy') # with the mag or vector method
-MEG_mmr1_m = np.load(root_path + 'meeg_mmr_analysis/group_mmr1_morph.npy') # with the mag or vector method
-MEG_mmr2_m = np.load(root_path + 'meeg_mmr_analysis/group_mmr2_morph.npy') # with the mag or vector method
+MEG_mmr1_v = np.load(root_path + 'cbsA_meeg_analysis/group_mmr1_vector_morph.npy') # with the mag or vector method
+MEG_mmr2_v = np.load(root_path + 'cbsA_meeg_analysis/group_mmr2_vector_morph.npy') # with the mag or vector method
+MEG_mmr1_m = np.load(root_path + 'cbsA_meeg_analysis/group_mmr1_morph.npy') # with the mag or vector method
+MEG_mmr2_m = np.load(root_path + 'cbsA_meeg_analysis/group_mmr2_morph.npy') # with the mag or vector method
 
-stc1.data = MEG_mmr1_m.mean(axis=0)
-stc2.data = MEG_mmr2_m.mean(axis=0)
-
-subject = 'fsaverage'
-src = mne.read_source_spaces(subjects_dir + subject + '/bem/fsaverage-vol-5-src.fif')
+stc1.data = MEG_mmr1_v.mean(axis=0)
+stc2.data = MEG_mmr2_v.mean(axis=0)
 
 # stcs = stc1.in_label(label = label_names[0], mri = fname_aseg, src = src) #restrict to stc to one of the label 
 # stc.plot(src,clim=dict(kind="value",pos_lims=[0,2,5]),subject=subject, subjects_dir=subjects_dir)
 # stc1.plot(src,subject=subject, subjects_dir=subjects_dir, bg_img='aparc+aseg.mgz', mode="glass_brain")
 # stc2.plot(src,subject=subject, subjects_dir=subjects_dir, bg_img='aparc+aseg.mgz')
+
+subject = 'fsaverage'
+src = mne.read_source_spaces(subjects_dir + subject + '/bem/fsaverage-vol-5-src.fif')
 
 ## stc of ROI
 fname_aseg = subjects_dir + subject + '/mri/aparc+aseg.mgz'
@@ -153,4 +153,55 @@ plt.legend(label_names[rh_ROI_label])
 plt.xlabel('Time (s)')
 plt.ylabel('Activation (AU)')
 plt.xlim([-0.1, 0.5])
+#    plt.savefig('/home/tzcheng/Desktop/' + 'run1_' + label[Qis_ROI_label[i]].name +'.pdf')
+
+#%%######################################## visualize the group level source average for INFANTS
+stc = mne.read_source_estimate(root_path + 'cbs_b106/sss_fif/cbs_b106_mmr2_None-vl.stc')
+
+MEG_mmr1_roi_v = np.load(root_path + 'cbsb_meg_analysis/group_mmr1_vector_roi.npy') # with the mag or vector method
+MEG_mmr2_roi_v = np.load(root_path + 'cbsb_meg_analysis/group_mmr2_vector_roi.npy') # with the mag or vector method
+MEG_mmr1_roi_m = np.load(root_path + 'cbsb_meg_analysis/group_mmr1_roi.npy') # with the mag or vector method
+MEG_mmr2_roi_m = np.load(root_path + 'cbsb_meg_analysis/group_mmr2_roi.npy') # with the mag or vector method
+
+MEG_mmr1_roi_v_mean = MEG_mmr1_roi_v.mean(axis=0)
+MEG_mmr2_roi_v_mean = MEG_mmr2_roi_v.mean(axis=0)
+MEG_mmr1_roi_m_mean = MEG_mmr1_roi_m.mean(axis=0)
+MEG_mmr2_roi_m_mean = MEG_mmr2_roi_m.mean(axis=0)
+
+## stc of ROI
+fname_aseg = subjects_dir + subject + '/mri/aparc+aseg.mgz'
+label_names = np.asarray(mne.get_volume_labels_from_aseg(fname_aseg))
+label_names
+
+# averaged whole brain mmr plot
+times = stc.times
+plt.figure()
+plot_err(MEG_mmr2_roi_m.mean(axis=1),'b',stc1.times)
+plot_err(MEG_mmr2_roi_v.mean(axis=1),'r',stc1.times)
+plt.legend(['MEG mag','','MEG vector',''])
+plt.title('mmr 2 averaged across ROIs')
+plt.xlabel('Time (ms)')
+plt.ylabel('zscore')
+plt.xlim([-100, 500])
+
+# ROI mmr plot
+lh_ROI_label = [49,50,51,61] # STG and IFG (parsopercularis, parsorbitalis, parstriangularis)
+rh_ROI_label = [84,85,86,96] # STG and IFG (parsopercularis, parsorbitalis, parstriangularis)
+
+plt.figure()
+plt.plot(times,MEG_mmr2_roi_v_mean[lh_ROI_label,].transpose())
+plt.title('CBS_b MMR2 lh')
+plt.legend(label_names[lh_ROI_label])
+plt.xlabel('Time (ms)')
+plt.ylabel('Activation (AU)')
+plt.xlim([-100, 500])
+#    plt.savefig('/home/tzcheng/Desktop/' + 'run1_' + label[Qis_ROI_label[i]].name +'.pdf')
+
+plt.figure()
+plt.plot(times,MEG_mmr2_roi_v_mean[rh_ROI_label,].transpose())
+plt.title('CBS_b MMR2 rh')
+plt.legend(label_names[rh_ROI_label])
+plt.xlabel('Time (ms)')
+plt.ylabel('Activation (AU)')
+plt.xlim([-100, 500])
 #    plt.savefig('/home/tzcheng/Desktop/' + 'run1_' + label[Qis_ROI_label[i]].name +'.pdf')
