4bda56649c504a4bb3522f94cbc8e501b5d65f92
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2023-10-12 17:04:07 -0700

    Update decoding_zoe.py

--- decoding_zoe.py
+++ decoding_zoe.py
@@ -10,14 +10,136 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import StratifiedKFold
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.svm import SVC
 
 import mne
 from mne.minimum_norm import apply_inverse_epochs, read_inverse_operator
-from mne.decoding import cross_val_multiscore, LinearModel, SlidingEstimator, get_coef
+from mne.preprocessing import Xdawn
+from mne.decoding import (
+    SlidingEstimator,
+    GeneralizingEstimator,
+    Scaler,
+    cross_val_multiscore,
+    LinearModel,
+    get_coef,
+    Vectorizer,
+    CSP,
+)
+#%%####################################### Trial-by-trial decoding for each individual
+subjects_dir = '/media/tzcheng/storage2/subjects/'
+root_path='/media/tzcheng/storage/CBS/'
+s = 'cbs_b112'
+file_in = root_path + '/' + s + '/sss_fif/' + s
 
+epochs = mne.read_epochs(file_in +'_01_otp_raw_sss_proj_fil50_mmr_e.fif')
+epochs.crop(0,0.3)
+epochs_d = epochs["Deviant1","Deviant2"]
+X = epochs_d.get_data()  # MEG signals features: n_epochs, n_meg_channels, n_times make sure the first dimension is epoch
+y = epochs_d.events[:, 2]  # target: standard, deviant1 and 2
+
+## spatial-temporal features
+# all sensors all time points (0 - 300 ms)
+clf = make_pipeline(
+    Scaler(epochs.info),
+    Vectorizer(),
+    LogisticRegression(solver="liblinear")  # liblinear is faster than lbfgs
+)
+scores = cross_val_multiscore(clf, X, y, cv=5, n_jobs=None) # takes about 10 mins to run
+score = np.mean(scores, axis=0)
+print("Spatio-temporal: %0.1f%%" % (100 * score,))
+
+clf = make_pipeline(
+    Scaler(epochs.info),
+    Vectorizer(),
+    SVC(kernel='rbf',gamma='auto')  
+)
+scores = cross_val_multiscore(clf, X, y, cv=5, n_jobs=None) # takes about 10 mins to run
+
+# Mean scores across cross-validation splits
+score = np.mean(scores, axis=0)
+print("Spatio-temporal: %0.1f%%" % (100 * score,))
+
+## SlidingEstimator
+clf = make_pipeline(
+    StandardScaler(), LinearModel(LogisticRegression(solver="liblinear"))
+)
+time_decod = SlidingEstimator(clf, n_jobs=None, scoring="roc_auc", verbose=True)
+scores = cross_val_multiscore(time_decod, X, y, cv=5, n_jobs=None)
+time_decod.fit(X, y)
+
+coef = get_coef(time_decod, "patterns_", inverse_transform=True)
+evoked_time_gen = mne.EvokedArray(coef, epochs.info, tmin=epochs.times[0])
+joint_kwargs = dict(ts_args=dict(time_unit="s"), topomap_args=dict(time_unit="s"))
+evoked_time_gen.plot_joint(
+    times=np.arange(0.100, 0.300, 0.05), title="patterns", **joint_kwargs
+)
+
+# Plot decoding accuracy for each time instance
+fig, ax = plt.subplots()
+ax.plot(epochs.times, np.mean(scores, axis=0), label="score")
+ax.axhline(0.5, color="k", linestyle="--", label="chance")
+ax.set_xlabel("Times")
+ax.set_ylabel("AUC")  # Area Under the Curve
+ax.legend()
+ax.axvline(0.0, color="k", linestyle="-")
+ax.set_title("Sensor space decoding")
+
+# Projecting sensor-space patterns to source space
+cov = mne.read_cov(file_in + '_01_erm_otp_raw_sss_proj_fil50-cov.fif')
+fwd = mne.read_forward_solution(file_in + '-fwd.fif')
+inv = mne.minimum_norm.make_inverse_operator(evoked_time_gen.info, fwd, cov, loose=1,depth=0.8)
+src = inv['src']
+stc = mne.minimum_norm.apply_inverse(evoked_time_gen, inv, 1.0 / 9.0, "dSPM")
+brain = stc.plot(src=src, subjects_dir=subjects_dir
+)
+
+## X-Dawn (doesn't work yet)
+# n_filter = 3
+# # Create classification pipeline
+# clf = make_pipeline(
+#     Xdawn(n_components=n_filter),
+#     Vectorizer(),
+#     MinMaxScaler(),
+#     LogisticRegression(solver="liblinear")
+# )
+
+# # Get the labels
+# labels = epochs.events[:, -1]
+
+# # Cross validator
+# cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+
+# # Do cross-validation
+# preds = np.empty(len(labels))
+# for train, test in cv.split(epochs, labels):
+#     clf.fit(epochs[train], labels[train])
+#     preds[test] = clf.predict(epochs[test])
+
+# # Classification report
+# target_names = ["Standard", "Deviant1", "Deviant2"]
+# report = classification_report(labels, preds, target_names=target_names)
+# print(report)
+
+# # Normalized confusion matrix
+# cm = confusion_matrix(labels, preds)
+# cm_normalized = cm.astype(float) / cm.sum(axis=1)[:, np.newaxis]
+
+# # Plot confusion matrix
+# fig, ax = plt.subplots(1, layout="constrained")
+# im = ax.imshow(cm_normalized, interpolation="nearest", cmap=plt.cm.Blues)
+# ax.set(title="Normalized Confusion matrix")
+# fig.colorbar(im)
+# tick_marks = np.arange(len(target_names))
+# plt.xticks(tick_marks, target_names, rotation=45)
+# plt.yticks(tick_marks, target_names)
+# ax.set(ylabel="True label", xlabel="Predicted label")
+
+#%%####################################### Subject-by-subject decoding for each condition
 #%%####################################### Load baby data
 root_path='/media/tzcheng/storage/CBS/'
 subjects_dir = '/media/tzcheng/storage2/subjects/'
