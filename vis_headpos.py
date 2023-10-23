bf853655d2c56894fb5833c3d983fd6b6a81d8ed
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2023-10-23 12:08:36 -0700

    Update vis_headpos.py
    Add comments

--- vis_headpos.py
+++ vis_headpos.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 8 11:28:12 2023
+
+For the purpose of comparing mne vs. mnefun
+
+@author: tzcheng
+"""
+
 ###### Import library 
 import mne
 import matplotlib
@@ -7,7 +16,4 @@ head_pos_mnefun = mne.chpi.read_head_pos('/media/tzcheng/storage/vmmr/vMMR_901/r
 head_pos_mne = mne.chpi.read_head_pos('/media/tzcheng/storage/vmmr/vMMR_901/raw_fif/vMMR_901_1_raw_mne.pos')
 
 mne.viz.plot_head_positions(head_pos_mnefun, mode="traces")
-%matplotlib qt
-
 mne.viz.plot_head_positions(head_pos_mne, mode="traces")
-%matplotlib qt
