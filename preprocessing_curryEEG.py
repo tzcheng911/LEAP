f829407f7f80c661ee2b4be367519a831a7be4ae
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2023-11-15 13:25:12 -0800

    Create preprocessing_curryEEG.py

---
+++ preprocessing_curryEEG.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 13:03:30 2023
+Used for analyzing Curry EEG system. 
+ 
+@author: tzcheng
+"""
+
+import mne 
+import matplotlib.pyplot as plt
+import numpy as np
+
+raw = mne.io.read_raw_curry('/home/tzcheng/Downloads/Acquisition 11.cdt', preload=True)
+events,event_id = mne.events_from_annotations(raw)
+raw_avg_ref = raw.copy().set_eeg_reference(ref_channels="average")
+raw_avg_ref.filter(l_freq=0,h_freq=50,method='iir',iir_params=dict(order=4,ftype='butter'))
+epoch = mne.Epochs(raw_avg_ref,events, event_id=1,tmin=-0.1,tmax=0.5,baseline=(-0.1,0))
+
