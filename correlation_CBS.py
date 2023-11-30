c807ecdf41ad45b7d9f7167bb04def6980232498
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2023-11-29 16:01:46 -0800

    FFR xcorr
    Group level and individual level

--- correlation_CBS.py
+++ correlation_CBS.py
@@ -379,50 +379,122 @@ print(max(r_all))
 print(min(r_all))
 
 #%%######################################## load EEG FFR
-## Load FFR
-std = np.load(root_path + 'cbsA_meeg_analysis/EEG/' + 'group_std_cabr_eeg_200.npy')
-dev1 = np.load(root_path + 'cbsA_meeg_analysis/EEG/' + 'group_dev1_cabr_eeg_200.npy')
-dev2 = np.load(root_path + 'cbsA_meeg_analysis/EEG/' + 'group_dev2_cabr_eeg_200.npy')
+## Load FFR from 0
+std = np.load(root_path + 'cbsA_meeg_analysis/EEG/' + 'group_std_cabr_eeg_200.npy')[:,100:]
+dev1 = np.load(root_path + 'cbsA_meeg_analysis/EEG/' + 'group_dev1_cabr_eeg_200.npy')[:,100:]
+dev2 = np.load(root_path + 'cbsA_meeg_analysis/EEG/' + 'group_dev2_cabr_eeg_200.npy')[:,100:]
 
 ## Load recorded  audio
-std_audio = np.load(root_path + 'stimuli/cbs_A123_ba.npy')
-dev1_audio = np.load(root_path + 'stimuli/cbs_A123_mba.npy')
-dev2_audio = np.load(root_path + 'stimuli/cbs_A123_pa.npy')
+# std_audio = np.load(root_path + 'stimuli/cbs_A123_ba.npy')[100:750]
+# dev1_audio = np.load(root_path + 'stimuli/cbs_A123_mba.npy')[100:750]
+# dev2_audio = np.load(root_path + 'stimuli/cbs_A123_pa.npy')[100:750]
 
-times = np.linspace(-0.02,0.2,1101)
+# plt.figure()
+# plt.plot(np.linspace(-0.02,0.2,1101),dev2.mean(axis=0))
+
+# plt.figure()
+# plt.plot(np.linspace(-0.02,0.2,1101),dev2_audio)
+
+# plt.figure()
+# plt.plot(np.linspace(0,0.13,650),dev2_audio_r)
+
 ## Load real audio
-# fs, std_audio = wavfile.read('/media/tzcheng/storage/CBS/stimuli/-10.wav')
-# fs, dev1_audio = wavfile.read('/media/tzcheng/storage/CBS/stimuli/+40.wav')
-# fs, dev2_audio = wavfile.read('/media/tzcheng/storage/CBS/stimuli/-40.wav')
+fs, std_audio = wavfile.read('/media/tzcheng/storage/CBS/stimuli/+10.wav')
+fs, dev1_audio = wavfile.read('/media/tzcheng/storage/CBS/stimuli/-40.wav')
+fs, dev2_audio = wavfile.read('/media/tzcheng/storage/CBS/stimuli/+40.wav')
 # Downsample
+fs_new = 5000
+num_std = int((len(std_audio)*fs_new)/fs)
+num_dev = int((len(dev2_audio)*fs_new)/fs)  # #sample_new/fs_new=#sample/fs find number of samples in the resampled data
+            
+std_audio = signal.resample(std_audio, num_std, t=None, axis=0, window=None)
+dev1_audio = signal.resample(dev1_audio, num_dev, t=None, axis=0, window=None)
+dev2_audio = signal.resample(dev2_audio, num_dev, t=None, axis=0, window=None)
 
+audio0 = std_audio
+EEG0 = std
+times0_audio = np.linspace(0,len(audio0)/5000,len(audio0))
+times0_eeg = np.linspace(0,len(EEG0[0])/5000,len(EEG0[0]))
+
+## dev 2
+ts = 100
+te = 650
+audio = audio0[ts:te] # try 0.05 to 0.13 s for dev2
+EEG = EEG0[:,ts:te]
+times_audio = times0_audio[ts:te]
+times_eeg = times0_eeg[ts:te]
+lags = signal.correlation_lags(len(audio),len(EEG[0]))
+lags_s = lags/5000
+
+## std
+ts = 100
+te = 500
+audio = audio0[ts:te] # try 0.03 to 0.1 s for std
+EEG = EEG0[:,ts:te]
+times_audio = times0_audio[ts:te]
+times_eeg = times0_eeg[ts:te]
+lags = signal.correlation_lags(len(audio),len(EEG[0]))
+lags_s = lags/5000
 
 ## Initialize 
 xcorr_all_s = []
 xcorr_lag_all_s = []
-times = np.linspace(-0.02,0.2,1101)
-audio = std_audio
-EEG = std
+
+
+plt.figure()
+plt.plot(times_audio,audio)
+
+plt.figure()
+plt.plot(times_eeg,EEG.mean(axis=0))
 
 a = (audio - np.mean(audio))/np.std(audio)
 a = a / np.linalg.norm(a)
 
+## For averaged results: raw audio
+b = (EEG.mean(axis=0) - np.mean(EEG.mean(axis=0)))/np.std(EEG.mean(axis=0))
+b = b / np.linalg.norm(b)
+xcorr = signal.correlate(a,b,mode='full')
+xcorr = abs(xcorr)
+xcorr_max = max(xcorr)
+xcorr_maxlag = np.argmax(xcorr)
+print("max lag: ", str(lags_s[xcorr_maxlag]*1000))
+print("max xcorr: ", str(xcorr_max))
+plt.figure()
+plt.plot(lags_s,xcorr)
+
+## For averaged results: rectified audio
+# r_audio = abs(audio)
+# a = (r_audio - np.mean(r_audio))/np.std(r_audio)
+# a = a / np.linalg.norm(a)
+# b = (EEG.mean(axis=0) - np.mean(EEG.mean(axis=0)))/np.std(EEG.mean(axis=0))
+# b = b / np.linalg.norm(b)
+# xcorr = signal.correlate(a,b,mode='full')
+# xcorr = abs(xcorr)
+# xcorr_max = max(xcorr)
+# xcorr_maxlag = np.argmax(xcorr)
+# lags_s[xcorr_maxlag]*1000
+
+## For each individual
 for s in np.arange(0,len(std),1):
     b = (EEG[s,:] - np.mean(EEG[s,:]))/np.std(EEG[s,:])
     
     ## the way matlab do xcorr normalization: the max is 1 if do a zero lag autocorrealtoin
     b = b / np.linalg.norm(b)
 
-    xcorr = signal.correlate(a,b)
+    xcorr = signal.correlate(a,b,mode='full')
     xcorr = abs(xcorr)
     xcorr_all_s.append(np.max(xcorr))
     xcorr_lag_all_s.append(np.argmax(xcorr))
-lags = signal.correlation_lags(len(a),len(b))
-lags_time = lags/5000
-    
-print('abs xcorr between FFR & audio:' + str(np.array(xcorr_all_s).mean()))
-print('abs xcorr lag between FFR & audio:' + str(np.array(lags_time[xcorr_lag_all_s]).mean()))
 
+print('abs xcorr between FFR & audio: ' + str(np.array(xcorr_all_s).mean()) + '(' + str(np.array(xcorr_all_s).std()) +')')
+print('abs xcorr lag between FFR & audio (ms): ' + str(np.array(lags_s[xcorr_lag_all_s]*1000).mean())+ '(' + str(np.array(lags_s[xcorr_lag_all_s]*1000).std()) +')')
+
+fig,axs = plt.subplots(len(EEG),1,sharex=True,sharey=True)
+fig.suptitle('FFR')
+
+for s in np.arange(0,len(EEG),1):
+    axs[s].plot(times_eeg,EEG[s,:])
+    
 dev1_xcorr
 dev1_xcorr_lag
 dev2_xcorr
