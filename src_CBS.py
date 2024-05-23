9093b8e54268e38aa9259b7ee34135a8817990f4
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-05-22 17:30:19 -0700

    Update src_CBS.py

--- src_CBS.py
+++ src_CBS.py
@@ -106,11 +106,11 @@ def do_inverse_cABR(s,run, morph):
 
     file_in = root_path + s + '/sss_fif/' + s
     fwd = mne.read_forward_solution(file_in + '-fwd.fif')
-    cov = mne.read_cov(file_in + run + '_erm_otp_raw_sss_proj_f_ffr-cov.fif')
-    epoch = mne.read_epochs(file_in + run + '_otp_raw_sss_proj_f_ffr_e_200.fif')
-    evoked_s = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_f_evoked_substd_ffr_200.fif')[0]
-    evoked_d1 = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_f_evoked_dev1_ffr_200.fif')[0]        
-    evoked_d2 = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_f_evoked_dev2_ffr_200.fif')[0]
+    cov = mne.read_cov(file_in + run + '_erm_otp_raw_sss_proj_f80450_ffr-cov.fif')
+    epoch = mne.read_epochs(file_in + run + '_otp_raw_sss_proj_f80450_ffr_e_200.fif')
+    evoked_s = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_f80450_evoked_substd_ffr_200.fif')[0]
+    evoked_d1 = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_f80450_evoked_dev1_ffr_200.fif')[0]        
+    evoked_d2 = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_f80450_evoked_dev2_ffr_200.fif')[0]
     
     inverse_operator = mne.minimum_norm.make_inverse_operator(epoch.info, fwd, cov,loose=1,depth=0.8)
 
@@ -134,9 +134,9 @@ def do_inverse_cABR(s,run, morph):
         standard_fsaverage = morph.apply(standard)
         dev1_fsaverage = morph.apply(dev1)
         dev2_fsaverage = morph.apply(dev2)
-        standard_fsaverage.save(file_in + '_ba_ffr_200_morph', overwrite=True)
-        dev1_fsaverage.save(file_in + '_mba_ffr_200_morph', overwrite=True)
-        dev2_fsaverage.save(file_in + '_pa_ffr_200_morph', overwrite=True)
+        standard_fsaverage.save(file_in + '_ba_ffr80450_200_morph', overwrite=True)
+        dev1_fsaverage.save(file_in + '_mba_ffr80450_200_morph', overwrite=True)
+        dev2_fsaverage.save(file_in + '_pa_ffr80450_200_morph', overwrite=True)
 
     else: 
         print('No morphing has been performed. The individual results may not be good to average.')
