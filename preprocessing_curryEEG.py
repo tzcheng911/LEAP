1d9604b36f85523e84a8ab0f3ac889b66b138dd1
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-06-18 12:10:39 -0700

    Update preprocessing_curryEEG.py

--- preprocessing_curryEEG.py
+++ preprocessing_curryEEG.py
@@ -11,12 +11,22 @@ import mne
 import matplotlib.pyplot as plt
 import numpy as np
 
-raw = mne.io.read_raw_curry('/home/tzcheng/Downloads/Acquisition_cz01.cdt', preload=True)
-raw = mne.io.read_raw_fif('/home/tzcheng/Downloads/irregular_cz.fif', preload=True)
+raw = mne.io.read_raw_curry('/home/tzcheng/Downloads/Acquisition_triggerTest 01.cdt', preload=True)
+raw.plot()
+# raw = mne.io.read_raw_fif('/home/tzcheng/Downloads/irregular_cz.fif', preload=True)
 
 events,event_id = mne.events_from_annotations(raw)
 raw_avg_ref = raw.copy().set_eeg_reference(ref_channels="average")
 raw_avg_ref.filter(l_freq=0,h_freq=50,method='iir',iir_params=dict(order=4,ftype='butter'))
 epoch = mne.Epochs(raw_avg_ref,events, event_id=1,tmin=-0.1,tmax=0.5,baseline=(-0.1,0))
+
 evoked = epoch.average()
 evoked.plot()
+
+## Do some artifact removal
+
+## check the stim2 and stimtracker timing
+events_test = events[events[:,2] != 1]
+diff = np.diff(events_test[:,0])/2000*1000 # difference showed in ms
+all_diff = diff[diff<=50]
+plt.hist(all_diff, bins =5)
