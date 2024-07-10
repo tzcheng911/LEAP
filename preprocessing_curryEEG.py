7a5bc8de0620dbbf362b32a17f89ef2c79296fb7
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-07-10 11:26:35 -0700

    Update preprocessing_curryEEG.py

--- preprocessing_curryEEG.py
+++ preprocessing_curryEEG.py
@@ -11,17 +11,19 @@ import mne
 import matplotlib.pyplot as plt
 import numpy as np
 
-raw = mne.io.read_raw_curry('/home/tzcheng/Downloads/Acquisition_triggerTest 01.cdt', preload=True)
-raw.plot()
+raw0 = mne.io.read_raw_curry('/media/tzcheng/storage/RASP/Acquisition20240628 01.cdt', preload=True)
+raw0.plot()
 # raw = mne.io.read_raw_fif('/home/tzcheng/Downloads/irregular_cz.fif', preload=True)
 
+raw = raw0.copy()
+raw.drop_channels(["FT11","Cz","CPz"])
 events,event_id = mne.events_from_annotations(raw)
-raw_avg_ref = raw.copy().set_eeg_reference(ref_channels="average")
+raw_avg_ref = raw.copy().set_eeg_reference(ref_channels="average",ch_type='eeg')
 raw_avg_ref.filter(l_freq=0,h_freq=50,method='iir',iir_params=dict(order=4,ftype='butter'))
 epoch = mne.Epochs(raw_avg_ref,events, event_id=1,tmin=-0.1,tmax=0.5,baseline=(-0.1,0))
 
 evoked = epoch.average()
-evoked.plot()
+evoked.plot_joint()
 
 ## Do some artifact removal
 
@@ -30,3 +32,6 @@ events_test = events[events[:,2] != 1]
 diff = np.diff(events_test[:,0])/2000*1000 # difference showed in ms
 all_diff = diff[diff<=50]
 plt.hist(all_diff, bins =5)
+
+## Visualization
+epoch.plot_image(picks="eeg", combine="mean")
