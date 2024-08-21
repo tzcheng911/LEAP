07381d200bc18f0412d12b9364452596001b30c3
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-08-21 13:36:00 -0700

    Update preprocessing_curryEEG.py

--- preprocessing_curryEEG.py
+++ preprocessing_curryEEG.py
@@ -10,7 +10,8 @@ Used for analyzing Curry EEG system.
 import mne 
 import matplotlib.pyplot as plt
 import numpy as np
-import curryreader as cr
+from scipy.spatial.transform import Rotation as R
+# import curryreader as cr
 
 root_path = '/media/tzcheng/storage/RASP/'
 
@@ -18,18 +19,29 @@ root_path = '/media/tzcheng/storage/RASP/'
 # currydata = cr.read(root_path + '20240815/Acquisition_Pilot4_clicks.cdt')
 
 ## load the data with read_raw_curry()
-raw0 = mne.io.read_raw_curry(root_path + '20240815/Acquisition_Pilot4_clicks.cdt', preload=True)
-montage = mne.channels.read_custom_montage(root_path + 'SynAmps2_Quik-Cap64.loc')
+raw0 = mne.io.read_raw_curry(root_path + '20240730/Acquisition_soundmod_Pilot3.cdt', preload=True) # Random and soundmod_Pilot3 showed more clear AEP
+
+## rotate the layout 180 degrees
+r = R.from_euler('z', 180, degrees=True)
+pos = np.zeros([64,3])
+pos_180 = np.zeros([64,3])
+for nch in np.arange(0,64,1):
+    tmp_pos = raw0.info['chs'][nch]['loc'][:3]
+    pos[nch,:] = tmp_pos
+    pos_180[nch,:] = r.apply(tmp_pos)
+    raw0.info['chs'][nch]['loc'][:3] = r.apply(tmp_pos)
+    
+# montage = mne.channels.read_custom_montage(root_path + 'SynAmps2_Quik-Cap64_curryreader.loc')
 # raw0.set_montage("standard_1020", match_case=False,on_missing='warn')
 # raw0.plot_sensors(show_names = True,sphere="eeglab")
 # raw0.plot()
 raw = raw0.copy()
-bad_channels = []
-raw.rename_channels({'Fp1':'FP1','Fp2':'FP2','Fpz':'FPZ','Cb1':'CB1','Cb2':'CB2',
-                     'Fz':'FZ','FCz':'FCZ','Cz':'CZ','CPz':'CPZ','Pz':'PZ',
-                     'POz':'POZ','Oz':'OZ','FT11':'FT7','FT12':'FT8'})
-raw.drop_channels(['Trigger','F11','F12','CB1','CB2'] + bad_channels)
-raw.set_montage(montage)
+# bad_channels = []
+# raw.rename_channels({'Fp1':'FP1','Fp2':'FP2','Fpz':'FPZ','Cb1':'CB1','Cb2':'CB2',
+#                      'Fz':'FZ','FCz':'FCZ','Cz':'CZ','CPz':'CPZ','Pz':'PZ',
+#                      'POz':'POZ','Oz':'OZ','FT11':'FT7','FT12':'FT8'})
+# raw.drop_channels(['Trigger','F11','F12','CB1','CB2'] + bad_channels)
+# raw.set_montage(montage)
 
 
 #%% Get events
@@ -59,7 +71,7 @@ events,event_id = mne.events_from_annotations(raw)
 # ica.plot_scores(eog_scores)
 # ica.plot_sources(raw, show_scrollbars=False)
 # ica.plot_components()
-# ica.plot_properties(raw, picks=[0, 1])
+# ica.plot_properties(raw, picks=[11])
 # ica.exclude = [0, 1]
 # reconst_raw = raw.copy()
 # ica.apply(reconst_raw)
@@ -85,7 +97,7 @@ raw_avg_ref.filter(l_freq=1,h_freq=50,method='iir',iir_params=dict(order=4,ftype
 # epoch = mne.Epochs(raw_avg_ref,events, event_id=1,tmin=-0.1,tmax=0.3,baseline=(-0.05,0), proj='delayed',reject=dict(eeg=100e-6))
 # evoked = epoch.average()
 # evoked.plot_topomap(proj='interactive')
-epoch = mne.Epochs(raw_avg_ref,events, event_id=1,tmin=-0.1,tmax=0.3,baseline=(-0.05,0), proj=True) # reject=dict(eeg=100e-6)
+epoch = mne.Epochs(raw_avg_ref,events, event_id=1,tmin=-0.1,tmax=0.5,baseline=(-0.05,0), proj=True,reject=dict(eeg=35-6)) # reject=dict(eeg=100e-6)
 evoked = epoch.average()
 #%% check the stim2 and stimtracker timing
 # events_test = events[events[:,2] != 1]
@@ -94,8 +106,8 @@ evoked = epoch.average()
 # plt.hist(all_diff, bins =5)
 
 #%% Visualization
-times = np.arange(-0.1, 0.2, 0.05)
+times = np.arange(-0.1, 0.5, 0.05)
 evoked.plot(ylim = dict(eeg=[-2, 2]))
-evoked.plot_topomap(times,vlim=(-1.5,1.5),show_names=False)
+evoked.plot_topomap(times,vlim=(-1,1),show_names=False)
 plt.figure()
 plt.plot(evoked.times,evoked.get_data().mean(0))
