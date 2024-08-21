860e8f609737f0bcfb9c1a05e1b1b113fdcfc9c5
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-08-21 13:35:55 -0700

    Update analysis_ME2.py

--- analysis_ME2.py
+++ analysis_ME2.py
@@ -19,6 +19,10 @@ from mne.datasets import somato
 from mne.time_frequency import tfr_morlet
 from scipy.io import wavfile
 from scipy import stats,signal
+from mne_connectivity import spectral_connectivity_epochs
+from mne_connectivity.viz import plot_connectivity_circle
+from mne.viz import circular_layout
+
 
 def plot_err(group_data,color,t):
     group_avg=np.mean(group_data,axis=0)
@@ -34,7 +38,7 @@ subjects_dir = '/media/tzcheng/storage2/subjects/'
 root_path='/media/tzcheng/storage/ME2_MEG/Zoe_analyses/'
 fname_aseg = subjects_dir + 'fsaverage/mri/aparc+aseg.mgz'
 label_names = np.asarray(mne.get_volume_labels_from_aseg(fname_aseg))
-nROI = [61,63] # Auditory (STG), Motor (precentral and paracentral), Basal ganglia () (left and then right)
+nROI = [61,63] # Auditory (STG 72,108), Motor (precentral 66 102 and paracentral 59, 95), Basal ganglia group (7,8,9,16,26,27,28,31) (left and then right)
 nV = 10020 # need to manually look up from the whole-brain plot
 
 fs, audio = wavfile.read(root_path + 'Stimuli/Random.wav') # Random, Duple300, Triple300
@@ -78,7 +82,7 @@ plt.figure()
 plot_err(psds[:,nROI,:],'k',freqs)
 
 psds, freqs = mne.time_frequency.psd_array_welch(
-MEG_v,MEG_fs, # could replace with label time series
+MEG_v,MEG_fs, # take about 2 mins
 n_fft=np.shape(MEG_v)[2],
 n_overlap=0,
 n_per_seg=None,
@@ -89,20 +93,29 @@ plt.figure()
 plot_err(psds[:,nV,:],'k',freqs)
 
 ## Whole-brain frequency spectrum
-stc1.data = MEG_v.mean(axis=0)
+stc1.data = psds.mean(axis=0)
+stc1.tmin = freqs[0] # hack into the time with freqs
+stc1.tstep = np.diff(freqs)[0] # hack into the time with freqs
 stc1.plot(src = src,clim=dict(kind="value",lims=[5,5.5,8]))
 
 #%%####################################### Connectivity analysis
-# Downsample
-fs_new = 1000
-num_random = int((len(random_audio)*fs_new)/fs)
-num_duple = int((len(duple_audio)*fs_new)/fs) 
-num_triple = int((len(triple_audio)*fs_new)/fs)  
-            
-random_audio = signal.resample(random_audio, num_random, t=None, axis=0, window=None)
-duple_audio = signal.resample(duple_audio, num_duple, t=None, axis=0, window=None)
-triple_audio = signal.resample(triple_audio, num_triple, t=None, axis=0, window=None)
+con_methods = ["pli", "wpli2_debiased", "ciplv"]
+con = spectral_connectivity_epochs(
+    MEG_roi,
+    method=con_methods,
+    mode="multitaper",
+    sfreq=MEG_fs,
+    fmin=fmin,
+    fmax=fmax,
+    faverage=True,
+    mt_adaptive=True,
+    n_jobs=1,
+)
+
+## visualization
 
 #%%##### 
-
-#%%####################################### ML sliding estimator
+# Downsample
+fs_new = 1000
+num = int((len(audio)*fs_new)/fs)        
+audio = signal.resample(audio, num, t=None, axis=0, window=None)
