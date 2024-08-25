50011c75f4f545570a8b951e15827a588a4e2687
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-08-24 19:32:04 -0700

    Update group_EEG_CBS.py

--- group_EEG_CBS.py
+++ group_EEG_CBS.py
@@ -27,7 +27,7 @@ for file in os.listdir():
         subj.append(file)
 
 runs = ['01', '02']
-run = runs[1]
+run = runs[0]
 
 # %% output the time series in npy files
 ## calculate the nave for each epoch here too
@@ -52,12 +52,12 @@ for s in subj:
 
     # Could do MMR or FFR
     # for the normal direction
-    # std = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_substd_mmr.fif', allow_maxshield=True)[0]
-    # last_ba_nave.append(std.nave)
-    # dev1 = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_dev1_mmr.fif', allow_maxshield=True)[0]
-    # first_mba_nave.append(dev1.nave)
-    # dev2 = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_dev2_mmr.fif', allow_maxshield=True)[0]
-    # first_pa_nave.append(dev2.nave)
+    std = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_substd_mmr.fif', allow_maxshield=True)[0]
+    last_ba_nave.append(std.nave)
+    dev1 = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_dev1_mmr.fif', allow_maxshield=True)[0]
+    first_mba_nave.append(dev1.nave)
+    dev2 = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_dev2_mmr.fif', allow_maxshield=True)[0]
+    first_pa_nave.append(dev2.nave)
     # std = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_substd_cabr_all.fif', allow_maxshield=True)[0]
     # dev1 = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_dev1_cabr_all.fif', allow_maxshield=True)[0]
     # dev2 = mne.read_evokeds(root_path + s + '/eeg/' + s + '_' + run + '_evoked_dev2_cabr_all.fif', allow_maxshield=True)[0]
@@ -72,7 +72,7 @@ for s in subj:
     std2 = mne.read_evokeds(root_path + s +'/eeg/' + s + '_' + run + '_evoked_substd2_reverse_mmr.fif',allow_maxshield = True)[0]
     last_mba_nave.append(std1.nave)
     last_pa_nave.append(std2.nave)
-    dev = mne.read_evokeds(root_path + s +'/eeg/' + s + '_' + run + '_evoked_dev_reverse_mmr.fif',allow_maxshield = True)[0]
+    # dev = mne.read_evokeds(root_path + s +'/eeg/' + s + '_' + run + '_evoked_dev_reverse_mmr.fif',allow_maxshield = True)[0]
     # mmr1 = dev.data - std1.data
     # mmr2 = dev.data - std2.data
 
@@ -85,29 +85,29 @@ for s in subj:
 
     # group_mmr1.append(mmr1)
     # group_mmr2.append(mmr2)
-    group_std1.append(std1.data)
-    group_std2.append(std2.data)
+    # group_std1.append(std1.data)
+    # group_std2.append(std2.data)
     # group_dev1.append(dev1.data)
     # group_dev2.append(dev2.data)
     # group_std.append(std.data)
-    group_dev.append(dev.data)
+    # group_dev.append(dev.data)
 
 
 # group_std = np.squeeze(np.asarray(group_std), 1)
 # group_dev1 = np.squeeze(np.asarray(group_dev1), 1)
 # group_dev2 = np.squeeze(np.asarray(group_dev2), 1)
-group_std1 = np.squeeze(np.asarray(group_std1),1)
-group_std2 = np.squeeze(np.asarray(group_std2),1)
+# group_std1 = np.squeeze(np.asarray(group_std1),1)
+# group_std2 = np.squeeze(np.asarray(group_std2),1)
 # group_mmr1 = np.squeeze(np.asarray(group_mmr1),1)
 # group_mmr2 = np.squeeze(np.asarray(group_mmr2),1)
-group_dev = np.squeeze(np.asarray(group_dev),1)
+# group_dev = np.squeeze(np.asarray(group_dev),1)
 # group_std = np.squeeze(np.asarray(group_std),1)
 
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_std_ba_eeg.npy',group_std)
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_mmr1_eeg.npy',group_mmr1)
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_mmr2_eeg.npy',group_mmr2)
-np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_std1_reverse_eeg.npy',group_std1)
-np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_std2_reverse_eeg.npy',group_std2)
+# np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_std1_reverse_eeg.npy',group_std1)
+# np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_std2_reverse_eeg.npy',group_std2)
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_std_ffr_eeg_all.npy', group_std)
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_dev1_ffr_eeg_all.npy', group_dev1)
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_dev2_ffr_eeg_all.npy', group_dev2)
@@ -116,4 +116,4 @@ np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_std2_reverse_eeg.npy',
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_dev2_eeg.npy', group_dev2)
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_dev1_ba_eeg.npy',group_dev1)
 # np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_dev2_ba_eeg.npy',group_dev2)
-np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_dev_reverse_eeg.npy',group_dev)
+# np.save(root_path + 'cbsA_meeg_analysis/group_' + run + '_dev_reverse_eeg.npy',group_dev)
