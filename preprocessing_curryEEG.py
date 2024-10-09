4c1096e75e00c312458689bc7392c110da896f76
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-10-09 13:21:54 -0700

    Update preprocessing_curryEEG.py

--- preprocessing_curryEEG.py
+++ preprocessing_curryEEG.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 15 13:03:30 2023
-Used for analyzing Curry EEG system. 
- 
+Preprocess EEG recorded in Curry EEG system. 
+1. visualize raw data (slow)
+2. extract events 
+3. ssp cleaning
+4. re-reference
+5. filtering
+5. epochs 
 @author: tzcheng
 """
 
@@ -19,7 +24,7 @@ root_path = '/media/tzcheng/storage/RASP/'
 # currydata = cr.read(root_path + '20240815/Acquisition_Pilot4_clicks.cdt')
 
 ## load the data with read_raw_curry()
-raw0 = mne.io.read_raw_curry(root_path + '20240730/Acquisition_soundmod_Pilot3.cdt', preload=True) # Random and soundmod_Pilot3 showed more clear AEP
+raw0 = mne.io.read_raw_curry(root_path + '20241008/Acquisition_pilot6.cdt', preload=True) # Random and soundmod_Pilot3 showed more clear AEP
 
 ## rotate the layout 180 degrees
 r = R.from_euler('z', 180, degrees=True)
@@ -31,21 +36,26 @@ for nch in np.arange(0,64,1):
     pos_180[nch,:] = r.apply(tmp_pos)
     raw0.info['chs'][nch]['loc'][:3] = r.apply(tmp_pos)
     
-# montage = mne.channels.read_custom_montage(root_path + 'SynAmps2_Quik-Cap64_curryreader.loc')
+montage = mne.channels.read_custom_montage(root_path + 'SynAmps2_Quik-Cap64_curryreader.loc')
 # raw0.set_montage("standard_1020", match_case=False,on_missing='warn')
 # raw0.plot_sensors(show_names = True,sphere="eeglab")
 # raw0.plot()
 raw = raw0.copy()
+raw.plot_sensors(show_names = True)
 # bad_channels = []
 # raw.rename_channels({'Fp1':'FP1','Fp2':'FP2','Fpz':'FPZ','Cb1':'CB1','Cb2':'CB2',
-#                      'Fz':'FZ','FCz':'FCZ','Cz':'CZ','CPz':'CPZ','Pz':'PZ',
-#                      'POz':'POZ','Oz':'OZ','FT11':'FT7','FT12':'FT8'})
+#                       'Fz':'FZ','FCz':'FCZ','Cz':'CZ','CPz':'CPZ','Pz':'PZ',
+#                       'POz':'POZ','Oz':'OZ','FT11':'FT7','FT12':'FT8'})
 # raw.drop_channels(['Trigger','F11','F12','CB1','CB2'] + bad_channels)
 # raw.set_montage(montage)
 
 
 #%% Get events
 events,event_id = mne.events_from_annotations(raw)
+raw.copy().pick(picks="EMG1").plot(
+         events=events,
+         color="gray",
+         event_color={1:"grey",5:"g",6:"b",7:"y"}) # plot and visualize the events
 
 #%% Do some artifact removal
 ## Repairing with ICA
@@ -77,15 +87,15 @@ events,event_id = mne.events_from_annotations(raw)
 # ica.apply(reconst_raw)
 
 ## Repairing with SSP
-# ecg_epochs = mne.preprocessing.create_ecg_epochs(raw,ch_name='EMG1').average()
-# ecg_epochs.plot_joint(times=[-0.25, -0.025, 0, 0.025, 0.25])
-# eog_epochs = mne.preprocessing.create_eog_epochs(raw,ch_name=['VEOG','HEOG']).average()
-# eog_epochs.plot_joint(times=[-0.25, -0.025, 0, 0.025, 0.25])
-# ecg_projs, ecg_events = mne.preprocessing.compute_proj_ecg(raw, ch_name='EMG1', reject=None)
-# eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, ch_name=['VEOG','HEOG'], reject=None)
+ecg_epochs = mne.preprocessing.create_ecg_epochs(raw,ch_name='EMG1').average()
+ecg_epochs.plot_joint(times=[-0.25, -0.025, 0, 0.025, 0.25])
+eog_epochs = mne.preprocessing.create_eog_epochs(raw,ch_name=['VEOG','HEOG']).average()
+eog_epochs.plot_joint(times=[-0.25, -0.025, 0, 0.025, 0.25])
+ecg_projs, ecg_events = mne.preprocessing.compute_proj_ecg(raw, ch_name='EMG1', reject=None)
+eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, ch_name=['VEOG','HEOG'], reject=None)
 
-# raw.add_proj(ecg_projs)
-# raw.add_proj(eog_projs)
+raw.add_proj(ecg_projs)
+raw.add_proj(eog_projs)
 
 #%% Referencing
 raw_avg_ref = raw.copy().set_eeg_reference(ref_channels="average",ch_type='eeg', projection=True)
@@ -97,8 +107,9 @@ raw_avg_ref.filter(l_freq=1,h_freq=50,method='iir',iir_params=dict(order=4,ftype
 # epoch = mne.Epochs(raw_avg_ref,events, event_id=1,tmin=-0.1,tmax=0.3,baseline=(-0.05,0), proj='delayed',reject=dict(eeg=100e-6))
 # evoked = epoch.average()
 # evoked.plot_topomap(proj='interactive')
-epoch = mne.Epochs(raw_avg_ref,events, event_id=1,tmin=-0.1,tmax=0.5,baseline=(-0.05,0), proj=True,reject=dict(eeg=35-6)) # reject=dict(eeg=100e-6)
+epoch = mne.Epochs(raw_avg_ref,events, event_id=7,tmin=-0.1,tmax=2,baseline=(-0.1,0), proj=True,reject=dict(eeg=35-6)) # reject=dict(eeg=100e-6)
 evoked = epoch.average()
+
 #%% check the stim2 and stimtracker timing
 # events_test = events[events[:,2] != 1]
 # diff = np.diff(events_test[:,0])/2000*1000 # difference showed in ms
