39b2ffe4895b30f0bce7ef4e784e1a6aa04360ce
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-10-17 15:23:05 -0700

    Update preprocessing_CBS_b.py

--- preprocessing_CBS_b.py
+++ preprocessing_CBS_b.py
@@ -141,6 +141,7 @@ def do_sss(subject,st_correlation,int_order,time):
     'sld_140': ['MEG0312', 'MEG1712'],
     'sld_142': ['MEG0312', 'MEG1712'],
     'sld_143': ['MEG0312', 'MEG1712'],
+    'sld_144': ['MEG0312', 'MEG1712', 'MEG2533']
     }
     
     t2_prebad = {
@@ -164,6 +165,7 @@ def do_sss(subject,st_correlation,int_order,time):
     'sld_124': ['MEG0312', 'MEG1712'],
     'sld_126': ['MEG0312', 'MEG1712'],
     'sld_127': ['MEG0312', 'MEG1712'],
+    'sld_128': ['MEG0312', 'MEG1712', 'MEG2533'],
     }
     
     t3_prebad = {
@@ -330,7 +332,7 @@ os.chdir(root_path)
 
 #%%## parameters 
 runs = ['_01'] # ['_01','_02'] for the adults and ['_01'] for the infants
-time = '_t1' # first time (6 mo) '_t1' or second time (12 mo) '_t2' or third time (14mo) '_t3' coming back, or 0 for cbs
+time = '_t2' # first time (6 mo) '_t1' or second time (12 mo) '_t2' or third time (14mo) '_t3' coming back, or 0 for cbs
 direction = "ba_to_pa"
 do_cabr = False # True: use the cABR filter, cov and epoch setting; False: use the MMR filter, cov and epoch setting
 st_correlation = 0.9 # 0.98 for adults and 0.9 for infants
@@ -341,7 +343,7 @@ subjects = []
 for file in os.listdir():
     if file.startswith('sld_105'): # cbs_b for the infants, sld for SLD infants
         subjects.append(file)
-subjects = ['sld_143']
+subjects = ['sld_128']
 
 #%%###### do the jobs
 for s in subjects:
