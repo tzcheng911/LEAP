11cddb9dec7cfa9c9904003ac17d04b688dc4e89
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-12-12 16:50:36 -0800

    Update group_ME2.py

--- group_ME2.py
+++ group_ME2.py
@@ -18,19 +18,19 @@ tmax = 9.0
 fmin = 0.5
 fmax = 5
 
-age = '11mo/' # '7mo/', '11mo/' or '' for adults br
+age = '' # '7mo/', '11mo/' or '' for adults br
 runs = ['_01','_02','_03','_04']
 resample_or_not = True
 rfs = 250
-root_path='/media/tzcheng/storage/ME2_MEG/Zoe_analyses/' # /media/tzcheng/storage/BabyRhythm/ for adults
-# root_path='/media/tzcheng/storage/BabyRhythm/'
+# root_path='/media/tzcheng/storage/ME2_MEG/Zoe_analyses/' 
+root_path='/media/tzcheng/storage/BabyRhythm/' # for adults
 subjects_dir = '/media/tzcheng/storage2/subjects/'
 os.chdir(root_path + age)
 subjects = []
 
 for file in os.listdir():
-    if file.startswith('me2_'): 
-    # if file.startswith('br_'): 
+    # if file.startswith('me2_'): 
+    if file.startswith('br_'): 
         subjects.append(file)
 
 for run in runs:
@@ -38,13 +38,13 @@ for run in runs:
     # output the sensor time series in npy files
     group = []
     
-    for s in subjects:
-        print('Extracting ' + s + ' data')
-        file_in = root_path + age + s + '/sss_fif/' + s + run
-        evoked = mne.read_evokeds(file_in + '_otp_raw_sss_proj_fil50_mag6pT_evoked.fif')[0]
-        group.append(evoked.data)
-    group=np.asarray(group)
-    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/11mo_group' + run + '_mag6pT_sensor.npy',group)
+    # for s in subjects:
+    #     print('Extracting ' + s + ' data')
+    #     file_in = root_path + age + s + '/sss_fif/' + s + run
+    #     evoked = mne.read_evokeds(file_in + '_otp_raw_sss_proj_fil50_mag6pT_evoked.fif')[0]
+    #     group.append(evoked.data)
+    # group=np.asarray(group)
+    # np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/11mo_group' + run + '_mag6pT_sensor.npy',group)
     # np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/br_group' + run + '_mag6pT_sensor.npy',group)
     
     #%% output the source time series in npy files
@@ -59,8 +59,8 @@ for run in runs:
     for s in subjects:
         print('Extracting ' + s + ' data')
         file_in = root_path + age + '/' + s + '/sss_fif/' + s + run    
-        stc_lcmv = mne.read_source_estimate(file_in+'_stc_lcmv_morph-vl.stc')
-        stc_mne = mne.read_source_estimate(file_in+'_stc_mne_morph-vl.stc')
+        stc_lcmv = mne.read_source_estimate(file_in+'_stc_lcmv_morph_mag6pT-vl.stc')
+        stc_mne = mne.read_source_estimate(file_in+'_stc_mne_morph_mag6pT-vl.stc')
         if resample_or_not:
             stc_lcmv.data = stc_lcmv.data.astype('float64') 
             stc_mne.data = stc_mne.data.astype('float64')
@@ -82,8 +82,7 @@ for run in runs:
     group_stc_lcmv_roi = np.asarray(group_stc_lcmv_roi)
     group_stc_mne_roi = np.asarray(group_stc_mne_roi)
             
-    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/' + age +'_group' + run + '_stc_rs_lcmv.npy',group_stc_lcmv)
-    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/' + age +'_group' + run + '_stc_rs_mne.npy',group_stc_mne)
-    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/7mo_15_32_group' + run + '_stc_lcmv_roi.npy',group_stc_lcmv_roi)
-    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/7mo_15_32_group' + run + '_stc_mne_roi.npy',group_stc_mne_roi)
-
+    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/' + age +'br_group' + run + '_stc_rs_lcmv_mag6pT_morph.npy',group_stc_lcmv)
+    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/' + age +'br_group' + run + '_stc_rs_mne_mag6pT_morph.npy',group_stc_mne)
+    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/' + age +'br_group' + run + '_stc_rs_lcmv_mag6pT_roi.npy',group_stc_lcmv_roi)
+    np.save('/media/tzcheng/storage/ME2_MEG/Zoe_analyses/me2_meg_analysis/' + age +'br_group' + run + '_stc_rs_mne_mag6pT_roi.npy',group_stc_mne_roi)
