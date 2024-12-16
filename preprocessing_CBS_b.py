95e952b2020474e016793a1cd286a1d991a6978d
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-12-16 14:16:25 -0800

    Update preprocessing_CBS_b.py

--- preprocessing_CBS_b.py
+++ preprocessing_CBS_b.py
@@ -348,7 +348,7 @@ os.chdir(root_path)
 
 #%%## parameters 
 runs = ['_01'] # ['_01','_02'] for the adults and ['_01'] for the infants
-time = '_t3' # first time (6 mo) '_t1' or second time (12 mo) '_t2' or third time (14mo) '_t3' coming back, or 0 for cbs
+time = '_t1' # first time (6 mo) '_t1' or second time (12 mo) '_t2' or third time (14mo) '_t3' coming back, or 0 for cbs
 direction = "ba_to_pa"
 do_cabr = False # True: use the cABR filter, cov and epoch setting; False: use the MMR filter, cov and epoch setting
 st_correlation = 0.9 # 0.98 for adults and 0.9 for infants
@@ -359,12 +359,12 @@ subjects = []
 for file in os.listdir():
     if file.startswith('sld_105'): # cbs_b for the infants, sld for SLD infants
         subjects.append(file)
-subjects = ['sld_129']
+subjects = ['sld_150']
 
 #%%###### do the jobs
 for s in subjects:
     print(s)
-    do_otp(s,time)
+    # do_otp(s,time)
     do_sss(s,st_correlation,int_order,time)
     for run in runs:
         if time == 0:
