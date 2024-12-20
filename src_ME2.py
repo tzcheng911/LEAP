6d7d1dffa4b28d0e48a4cebcf1535e80e9b4efb2
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2024-12-20 13:06:49 -0800

    Update src_ME2.py

--- src_ME2.py
+++ src_ME2.py
@@ -31,21 +31,21 @@ def do_foward(s):
 
     return fwd, src
 
-def do_inverse(s,morph,run):
+def do_inverse(s,morph,run,rfs):
     # root_path='/media/tzcheng/storage/ME2_MEG/Zoe_analyses/11mo/'
     root_path = '/media/tzcheng/storage/BabyRhythm/'
     subjects_dir = '/media/tzcheng/storage2/subjects/'
     file_in = root_path + s + '/sss_fif/' + s
     fwd = mne.read_forward_solution(file_in + '-fwd.fif')
-    epoch = mne.read_epochs(file_in + run + '_otp_raw_sss_proj_fil50_epoch.fif')
+    epoch = mne.read_epochs(file_in + run + '_otp_raw_sss_proj_fil50_epoch.fif').resample(sfreq = rfs)
     noise_cov = mne.read_cov(file_in + run + '_erm_otp_raw_sss_proj_fil50-cov.fif')
     data_cov = mne.compute_covariance(epoch, tmin=0, tmax=None)
-    # evoked = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_fil50_mag6pT_evoked.fif')[0]
-    evoked_random_duple = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_fil50_mag6pT_evoked_randduple.fif')[0]
-    evoked_random_triple = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_fil50_mag6pT_evoked_randtriple.fif')[0]
+    evoked = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_fil50_mag6pT_evoked.fif')[0].resample(sfreq = rfs)
+    # evoked_random_duple = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_fil50_mag6pT_evoked_randduple.fif')[0]
+    # evoked_random_triple = mne.read_evokeds(file_in + run + '_otp_raw_sss_proj_fil50_mag6pT_evoked_randtriple.fif')[0]
     
-    evoked_random_duple = evoked_random_duple.resample(sfreq = 250)
-    evoked_random_triple = evoked_random_triple.resample(sfreq = 250)
+    # evoked_random_duple = evoked_random_duple.resample(sfreq = 250)
+    # evoked_random_triple = evoked_random_triple.resample(sfreq = 250)
     ## can experiment on pick_ori
 #     filters = make_lcmv(
 #     evoked.info,
@@ -62,9 +62,9 @@ def do_inverse(s,morph,run):
 #     src = fwd['src']
    
     inverse_operator = mne.minimum_norm.make_inverse_operator(epoch.info, fwd, noise_cov,loose=1,depth=0.8)
-    # stc_mne = mne.minimum_norm.apply_inverse((evoked), inverse_operator, pick_ori = None)
-    stc_mne_random_duple = mne.minimum_norm.apply_inverse((evoked_random_duple), inverse_operator, pick_ori = None)
-    stc_mne_random_triple = mne.minimum_norm.apply_inverse((evoked_random_triple), inverse_operator, pick_ori = None)
+    stc_mne = mne.minimum_norm.apply_inverse((evoked), inverse_operator, pick_ori = None)
+    # stc_mne_random_duple = mne.minimum_norm.apply_inverse((evoked_random_duple), inverse_operator, pick_ori = None)
+    # stc_mne_random_triple = mne.minimum_norm.apply_inverse((evoked_random_triple), inverse_operator, pick_ori = None)
 
     if morph == True:
         print('Morph ' + s +  ' src space to common cortical space.')
@@ -80,12 +80,12 @@ def do_inverse(s,morph,run):
             verbose=True)
         # stc_lcmv_fsaverage = morph.apply(stc_lcmv)
         # stc_lcmv_fsaverage.save(file_in + run + '_stc_lcmv_morph_mag6pT', overwrite=True)
-        # stc_mne_fsaverage = morph.apply(stc_mne)
-        # stc_mne_fsaverage.save(file_in + run + '_stc_mne_morph_mag6pT', overwrite=True)
-        stc_mne_random_duple_fsaverage = morph.apply(stc_mne_random_duple)
-        stc_mne_random_duple_fsaverage.save(file_in + run + '_stc_mne_morph_mag6pT_randduple_rs', overwrite=True)
-        stc_mne_random_triple_fsaverage = morph.apply(stc_mne_random_triple)
-        stc_mne_random_triple_fsaverage.save(file_in + run + '_stc_mne_morph_mag6pT_randtriple_rs', overwrite=True)
+        stc_mne_fsaverage = morph.apply(stc_mne)
+        stc_mne_fsaverage.save(file_in + run + '_stc_mne_morph_mag6pT', overwrite=True)
+        # stc_mne_random_duple_fsaverage = morph.apply(stc_mne_random_duple)
+        # stc_mne_random_duple_fsaverage.save(file_in + run + '_stc_mne_morph_mag6pT_randduple_rs', overwrite=True)
+        # stc_mne_random_triple_fsaverage = morph.apply(stc_mne_random_triple)
+        # stc_mne_random_triple_fsaverage.save(file_in + run + '_stc_mne_morph_mag6pT_randtriple_rs', overwrite=True)
     else: 
         print('No morphing has been performed. The individual results may not be good to average.')
         # stc_lcmv.save(file_in + '_stc_lcmv', overwrite=True)
@@ -99,6 +99,7 @@ def do_inverse(s,morph,run):
 # root_path='/media/tzcheng/storage/ME2_MEG/Zoe_analyses/11mo/' # change to 11mo and /media/tzcheng/storage/BabyRhythm/
 root_path = '/media/tzcheng/storage/BabyRhythm/'
 os.chdir(root_path)
+rfs=250
 
 morph = True
 
@@ -113,4 +114,4 @@ for s in tqdm(subj):
     # do_foward(s)
     for run in runs:
         print(s)
-        do_inverse(s,morph,run)
+        do_inverse(s,morph,run,rfs)
