9c68a09bc7a1b002d6345a7c30bc60622759cbf4
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2025-01-13 16:51:10 -0800

    Update evtag_CBS_b.py

--- evtag_CBS_b.py
+++ evtag_CBS_b.py
@@ -283,8 +283,8 @@ for file in os.listdir():
         subj.append(file)
 
 ## do individual by individual(s), check the time t1, t2 or t3 too
-subj = ['sld_150']
-conditions = ['6']
+subj = ['sld_152']
+conditions = ['4']
 ###### do the jobs
 for n,s in enumerate(subj):
     condition = conditions[n]
