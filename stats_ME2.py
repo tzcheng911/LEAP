daeb3bdbc0996ce98b1486bea84a349c93596394
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2025-01-15 15:49:22 -0800

    Update stats_ME2.py

--- stats_ME2.py
+++ stats_ME2.py
@@ -102,7 +102,55 @@ def wholebrain_spatio_temporal_cluster_test(X,n_meter,n_age,n_folder,p_threshold
     filename = root_path + n_folder + n_age + '_SSEP_wholebrain_cluster_test_' + n_meter +'.pkl'
     with open(filename, 'wb') as f:
         pickle.dump(clu, f) # clu: clustering results of T_obs, clusters, cluster_p_values, H0
-        
+
+def convert_dConn_to_csv(data_type,ROIs,n_analysis,n_folder):
+    lm_np = []
+    sub_col = [] 
+    age_col = []
+    cond_col = []
+    ROI_col = []
+    ages = ['7mo','11mo','br'] 
+    conditions = ['_02','_03','_04']
+    FOI = ['Delta', 'Theta', 'Alpha', 'Beta']
+
+    subj_path=['/media/tzcheng/storage/ME2_MEG/Zoe_analyses/7mo/' ,
+               '/media/tzcheng/storage/ME2_MEG/Zoe_analyses/11mo/',
+               '/media/tzcheng/storage/BabyRhythm/']
+         
+    for n_age,age in enumerate(ages):
+        print(age)
+        for n_cond,cond in enumerate(conditions):
+            print(cond)
+            data0 = read_connectivity(root_path + n_folder + age + '_group' + cond + '_stc_rs_mne_mag6pT' + data_type + n_analysis) 
+            freqs = data0.freqs
+            data0_conn = np.squeeze(data0.get_data(output='compact'))
+            print(np.shape(data0_conn))
+            data = np.vstack((data0_conn[:,ff(freqs,1):ff(freqs,4)].mean(axis=-1),
+                              data0_conn[:,ff(freqs,4):ff(freqs,8)].mean(axis=-1),
+                              data0_conn[:,ff(freqs,8):ff(freqs,12)].mean(axis=-1),
+                              data0_conn[:,ff(freqs,15):ff(freqs,30)].mean(axis=-1),
+                              data0_conn.mean(axis=-1))).transpose() # delta, theta, alpha, beta total 4 cols
+            lm_np.append(data)
+            if age == 'br':
+                for file in os.listdir(subj_path[n_age]):
+                    if file.startswith('br_'):
+                        sub_col.append(file)
+                        cond_col.append(cond)
+                        age_col.append(age)
+            else:
+                for file in os.listdir(subj_path[n_age]):
+                    if file.startswith('me2_'):
+                        sub_col.append(file)
+                        cond_col.append(cond)
+                        age_col.append(age)
+    lm_df = pd.DataFrame({'sub_id': sub_col,'age':age_col,'condition':cond_col,
+                          'Delta conn': np.concatenate(lm_np)[:,0], 
+                          'Theta conn': np.concatenate(lm_np)[:,1],
+                          'Alpha conn': np.concatenate(lm_np)[:,2],
+                          'Beta conn': np.concatenate(lm_np)[:,3],
+                          'Broadband conn': np.concatenate(lm_np)[:,4]})
+    lm_df.to_csv(root_path + n_folder + data_type + n_analysis + '.csv')
+    
 def convert_Conn_to_csv(data_type,ROIs,n_analysis,n_folder,ROI1,ROI2):
     lm_np = []
     sub_col = [] 
@@ -268,7 +316,7 @@ subjects_dir = '/media/tzcheng/storage2/subjects/'
 conditions = ['_02','_03','_04']
 ages = ['7mo','11mo','br'] 
 folders = ['SSEP/','ERSP/','decoding/','connectivity/'] # random, duple, triple
-analysis = ['psds','decoding_acc_perm100','conn_plv','conn_coh','conn_pli']
+analysis = ['psds','decoding_acc_perm100','conn_plv','conn_coh','conn_pli','conn_GC_AM','conn_GC_MA']
 which_data_type = ['_sensor_','_roi_','_roi_redo5_','_morph_'] ## currently not able to run ERSP and conn on the wholebrain data
 
 #%%####################################### Analysis on the sensor level 
@@ -276,7 +324,7 @@ data_type = which_data_type[0]
 n_analysis = analysis[0]
 n_folder = folders[0]
 
-for n_age in age:
+for n_age in ages:
     print("Doing age " + n_age)
     random = np.load(root_path + n_folder + n_age + '_group_02_rs_mag6pT' + data_type + n_analysis +'.npz') 
     duple = np.load(root_path + n_folder + n_age + '_group_03_rs_mag6pT' + data_type + n_analysis + '.npz') 
@@ -298,7 +346,7 @@ for n_age in age:
 
 #%%####################################### Analysis on the source level: ROI 
 n_folder = folders[3]
-n_analysis = analysis[3]
+n_analysis = analysis[2]
 data_type = which_data_type[2]
 
 nlines = 10
@@ -307,7 +355,7 @@ fname_aseg = subjects_dir + 'fsaverage/mri/aparc+aseg.mgz'
 if data_type == '_roi_':
     label_names = np.asarray(mne.get_volume_labels_from_aseg(fname_aseg))
     nROI = [72,108,66,102,64,100,59,95,7,8,26,27,60,61,62,96,97,98,50,86,71,107] 
-elif data_type == '_roi_redo_':
+elif data_type == '_roi_redo5_':
     # label_names = np.asarray(["AuditoryL", "AuditoryR", "MotorL", "MotorR", "SensoryL", "SensoryR", "BGL", "BGR", "IFGL", "IFGR"])
     label_names = np.asarray(["Auditory", "Motor", "Sensory", "BG", "IFG"])
     nROI = np.arange(0,len(label_names),1)
@@ -319,6 +367,7 @@ elif data_type == '_roi_redo_':
 # roi_redo pools ROIs to be 6 new_ROIs = {"Auditory": [72,108], "Motor": [66,102], "Sensory": [64,100], "BG": [7,8,26,27], "IFG": [60,61,62,96,97,98],  "Posterior": [50,86,71,107]}
 
 if n_folder == 'connectivity/':
+    # convert_dConn_to_csv(data_type,label_names,n_analysis,n_folder) # Connectivity between Auditory & Motor 
     convert_Conn_to_csv(data_type,label_names,n_analysis,n_folder,1,0) # Connectivity between Auditory & Motor 
     for n_age in ages:
         print("Doing connectivity " + n_age)
@@ -424,41 +473,53 @@ CDI_WS_11mo = CDI_WS0[CDI_WS0['ParticipantId'].isin(subj_11mo)] # select the sub
 
 ######## decide variables for correlation
 CDI_score = 'VOCAB' # WG('UWORDS','PWORDS','UWRDPER','PWRDPER'), WS('M3L','VOCAB')
-CDIAge = 24 # WG(12, 15), WS(18,21,24,27,30)
-# CDI = CDI_WG_7mo[CDI_WG_7mo['CDIAge'] == CDIAge][CDI_score]
-CDI = CDI_WS_7mo[CDI_WS_7mo['CDIAge'] == CDIAge][CDI_score]
+CDIAge = 27 # WG(12, 15), WS(18,21,24,27,30) me2_313_11m has missing 24 mo WS data, do 27 instead
 
 # Subjects who has neural data but does not have CDI data: 7mo ('me2_203', 'me2_120', 'me2_117')
 subj_noCDI = list(set(subj_7mo) - set(CDI_WG0['ParticipantId'])) # same result in list(set(subj_all) - set(CDI_WS0['ParticipantId']))   
 subj_noCDI_ind = [2,8,25] # CAUTION hardcoded manual input here, check if this is the data storing order for 7mo in group_ME2.py (confirmed 2025/1/13 Zoe)
 
 ## Neural measurements: conn
-age = '7mo'
-for n_cond,cond in enumerate(conditions):
-    conn0 = read_connectivity(root_path + 'connectivity/' + age + '_group' + cond + '_stc_rs_mne_mag6pT' + data_type + 'conn_plv') 
-    conn = conn0.get_data(output='dense')
-    conn = np.delete(conn,subj_noCDI_ind,axis=0) # delete the 3 subjects 'me2_203', 'me2_120', 'me2_117' who don't have CDI
-    conn_theta = conn[:,2,1,ff(conn0.freqs,4):ff(conn0.freqs,8)].mean(-1)
-    conn_alpha = conn[:,2,1,ff(conn0.freqs,8):ff(conn0.freqs,12)].mean(-1)
-    
-    plt.figure()
-    plt.scatter(conn_theta,CDI)
-    print(pearsonr(conn_theta, CDI))
-    print(pearsonr(conn_alpha, CDI))
-
-
 age = '11mo'
 for n_cond,cond in enumerate(conditions):
     conn0 = read_connectivity(root_path + 'connectivity/' + age + '_group' + cond + '_stc_rs_mne_mag6pT' + data_type + 'conn_plv') 
     conn = conn0.get_data(output='dense')
-    conn0.freqs
+    if age == '7mo':
+        conn = np.delete(conn,subj_noCDI_ind,axis=0) # delete the 3 subjects 'me2_203', 'me2_120', 'me2_117' who don't have CDI
+        CDI = CDI_WS_7mo[CDI_WS_7mo['CDIAge'] == CDIAge][CDI_score]
+        # CDI = CDI_WG_7mo[CDI_WG_7mo['CDIAge'] == CDIAge][CDI_score]
+    else:
+        CDI = CDI_WS_11mo[CDI_WS_11mo['CDIAge'] == CDIAge][CDI_score]
+
+    conn_theta = conn[:,2,1,ff(conn0.freqs,4):ff(conn0.freqs,8)].mean(-1)
+    conn_alpha = conn[:,2,1,ff(conn0.freqs,8):ff(conn0.freqs,12)].mean(-1)
+    
+    # plt.figure()
+    # plt.scatter(conn_theta,CDI)
+    print(pearsonr(conn_theta, CDI))
+    print(pearsonr(conn_alpha, CDI))
 
 ## Neural measurements: SSEP
-for n in nROI: 
-    for n_cond,cond in enumerate(conditions):
-        SSEP0 = np.load(root_path + 'SSEP/' + age + '_group' + cond + '_stc_rs_mne_mag6pT' + data_type +'psds.npz') 
-        SSEP = SSEP0[SSEP0.files[0]]
-        psds[psds.files[1]] 
+for n_cond,cond in enumerate(conditions):
+    SSEP0 = np.load(root_path + 'SSEP/' + age + '_group' + cond + '_stc_rs_mne_mag6pT' + data_type +'psds.npz') 
+    SSEP = SSEP0[SSEP0.files[0]]
+    if age == '7mo':
+        SSEP = np.delete(SSEP,subj_noCDI_ind,axis=0) # delete the 3 subjects 'me2_203', 'me2_120', 'me2_117' who don't have CDI
+        CDI = CDI_WS_7mo[CDI_WS_7mo['CDIAge'] == CDIAge][CDI_score]
+        # CDI = CDI_WG_7mo[CDI_WG_7mo['CDIAge'] == CDIAge][CDI_score]
+    else:
+        CDI = CDI_WS_11mo[CDI_WS_11mo['CDIAge'] == CDIAge][CDI_score]
         
-## calculate correlation
-corr_p = pearsonr(MEG, CDI_WS)
+    SSEP_triple = SSEP[:,:,ff(SSEP0[SSEP0.files[1]],1.11)]
+    SSEP_duple = SSEP[:,:,ff(SSEP0[SSEP0.files[1]],1.67)]
+    SSEP_beat = SSEP[:,:,ff(SSEP0[SSEP0.files[1]],3.33)]
+    
+    for nROI,ROI in enumerate(label_names):
+        # plt.figure()
+        # plt.scatter(conn_theta,CDI)
+        print(cond + ' 1.11 Hz ' + ROI)
+        print(pearsonr(SSEP_triple[:,nROI], CDI))
+        print(cond + ' 1.67 Hz ' + ROI)  
+        print(pearsonr(SSEP_duple[:,nROI], CDI))
+        print(cond + ' 3.33 Hz ' + ROI)
+        print(pearsonr(SSEP_beat[:,nROI], CDI))
