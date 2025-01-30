f6304397b6e512913d37f333a4c5961022b4c80d
Tzu-Han Zoe Cheng <31935249+tzcheng911@users.noreply.github.com>
2025-01-30 15:45:06 -0800

    Update lm_ME2_conn.R

--- ME2_LMM/script/lm_ME2_conn.R
+++ ME2_LMM/script/lm_ME2_conn.R
@@ -16,9 +16,9 @@ alldataL = read.csv("lAM_roi_redo10_conn_plv.csv")
 alldataR = read.csv("rAM_roi_redo10_conn_plv.csv")
 alldata = read.csv("roi_redo5_conn_GC_MA.csv")
 alldata = read.csv("roi_redo5_conn_GC_M_seed_MI.csv")
-alldata = read.csv("conn_plv_roi_redo5_IB.csv")
+alldata = read.csv("conn_plv_roi_redo4_SA.csv")
 
-# log transform to achieve normality
+# log transform to achieve normality 
 alldata = alldata %>% 
   mutate(log_Delta.conn = log(Delta.conn),log_Theta.conn = log(Theta.conn), log_Alpha.conn = log(Alpha.conn), log_Beta.conn = log(Beta.conn))
 
@@ -45,19 +45,19 @@ summary_alldata = alldata %>%
 duple_random = filter(alldata,condition != "_04")
 triple_random = filter(alldata,condition != "_03")
 
-summary(lmer(log_Delta.conn ~ 1+ condition*age + (1|sub_id),data=duple_random))
-summary(lmer(log_Theta.conn ~ 1+ condition*age + (1|sub_id),data=duple_random))
-summary(lmer(log_Alpha.conn ~ 1+ condition*age + (1|sub_id),data=duple_random))
-summary(lmer(log_Beta.conn ~ 1+ condition*age + (1|sub_id),data=duple_random))
+summary(lmer(Delta.conn ~ 1+ condition*age + (1|sub_id),data=duple_random))
+summary(lmer(Theta.conn ~ 1+ condition*age + (1|sub_id),data=duple_random))
+summary(lmer(Alpha.conn ~ 1+ condition*age + (1|sub_id),data=duple_random))
+summary(lmer(Beta.conn ~ 1+ condition*age + (1|sub_id),data=duple_random))
 
 # notes for duple: even before log transform residual is pretty normal for theta, alpha and beta
-lm = lmer(Beta.conn ~ 1+ condition*age + (1|sub_id),data=duple_random)
+lm = lmer(Theta.conn ~ 1+ condition*age + (1|sub_id),data=duple_random)
 hist(residuals(lm))
 
-summary(lmer(log_Delta.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
-summary(lmer(log_Theta.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
-summary(lmer(log_Alpha.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
-summary(lmer(log_Beta.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
+summary(lmer(Delta.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
+summary(lmer(Theta.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
+summary(lmer(Alpha.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
+summary(lmer(Beta.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
 
 # notes for triple: even before log transform residual is pretty normal for theta, alpha and beta
 lm = lmer(log_Alpha.conn ~ 1+ condition*age + (1|sub_id),data=triple_random)
@@ -74,11 +74,25 @@ summary(lmer(Alpha.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
 summary(lmer(Beta.conn ~ 1+ condition*age + (1|sub_id),data=triple_random))
 
 ## Visualization
-ggplot(duple_random, aes(x = age, y = Delta.conn, fill = condition)) +
+ggplot(alldata, aes(x = age, y = Theta.conn, fill = condition)) +
   geom_bar(stat="summary", position='dodge') +
   stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
-  geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
+  geom_point(position = position_jitterdodge(jitter.width = 0.1,dodge.width = 0.9), color="black")+
+  ylim(0,1)+
   theme_bw()
+
+ggplot(alldata, aes(x = age, y = Alpha.conn, fill = condition)) +
+  geom_bar(stat="summary", position='dodge') +
+  stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
+  geom_point(position = position_jitterdodge(jitter.width = 0.1,dodge.width = 0.9), color="black")+
+  ylim(0,1)+
+  theme_bw()
+
+# ggplot(duple_random, aes(x = age, y = Delta.conn, fill = condition)) +
+#   geom_bar(stat="summary", position='dodge') +
+#   stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
+#   geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
+#   theme_bw()
 
 ggplot(duple_random, aes(x = age, y = Theta.conn, fill = condition)) +
   geom_bar(stat="summary", position='dodge') +
@@ -92,23 +106,23 @@ ggplot(duple_random, aes(x = age, y = Alpha.conn, fill = condition)) +
   geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
   theme_bw()
 
-ggplot(duple_random, aes(x = age, y = Beta.conn, fill = condition)) +
-  geom_bar(stat="summary", position='dodge') +
-  stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
-  geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
-  theme_bw()
-
-ggplot(duple_random, aes(x = age, y = Broadband.conn, fill = condition)) +
-  geom_bar(stat="summary", position='dodge') +
-  stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
-  geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
-  theme_bw()
-
-ggplot(triple_random, aes(x = age, y = Delta.conn, fill = condition)) +
-  geom_bar(stat="summary", position='dodge') +
-  stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
-  geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
-  theme_bw()
+# ggplot(duple_random, aes(x = age, y = Beta.conn, fill = condition)) +
+#   geom_bar(stat="summary", position='dodge') +
+#   stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
+#   geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
+#   theme_bw()
+# 
+# ggplot(duple_random, aes(x = age, y = Broadband.conn, fill = condition)) +
+#   geom_bar(stat="summary", position='dodge') +
+#   stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
+#   geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
+#   theme_bw()
+# 
+# ggplot(triple_random, aes(x = age, y = Delta.conn, fill = condition)) +
+#   geom_bar(stat="summary", position='dodge') +
+#   stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
+#   geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
+#   theme_bw()
 
 ggplot(triple_random, aes(x = age, y = Theta.conn, fill = condition)) +
   geom_bar(stat="summary", position='dodge') +
@@ -122,15 +136,15 @@ ggplot(triple_random, aes(x = age, y = Alpha.conn, fill = condition)) +
   geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
   theme_bw()
 
-ggplot(triple_random, aes(x = age, y = Beta.conn, fill = condition)) +
-  geom_bar(stat="summary", position='dodge') +
-  stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
-  geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
-  theme_bw()
-
-ggplot(triple_random, aes(x = age, y = Broadband.conn, fill = condition)) +
-  geom_bar(stat="summary", position='dodge') +
-  stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
-  geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
-  theme_bw()
+# ggplot(triple_random, aes(x = age, y = Beta.conn, fill = condition)) +
+#   geom_bar(stat="summary", position='dodge') +
+#   stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
+#   geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
+#   theme_bw()
+# 
+# ggplot(triple_random, aes(x = age, y = Broadband.conn, fill = condition)) +
+#   geom_bar(stat="summary", position='dodge') +
+#   stat_summary(fun.data=mean_se, geom="errorbar", position = position_dodge(width = 0.9), width=.1,color="grey") +
+#   geom_point(position = position_jitterdodge(jitter.width = 0.3,dodge.width = 0.9), color="black")+
+#   theme_bw()
 
