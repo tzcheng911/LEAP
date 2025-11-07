f17a85ebdd45c9ee9e32c005cae4c2e8ab405b78
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2025-11-06 21:47:54 -0800

    Update preprocessing_CBS_b.py

--- preprocessing_CBS_b.py
+++ preprocessing_CBS_b.py
@@ -206,7 +206,8 @@ def do_sss(subject,st_correlation,int_order,time):
     'sld_150': ['MEG0323', 'MEG0713','MEG0733'],
     'sld_152': ['MEG0323', 'MEG0713','MEG0733'],
     'sld_153': ['MEG2533'],
-    'sld_154': ['MEG1033', 'MEG2141','MEG2533']
+    'sld_154': ['MEG1033', 'MEG2141','MEG2533'],
+    'sld_157': ['MEG1043', 'MEG1242']
     }
     
     t3_prebad = {
@@ -395,18 +396,19 @@ os.chdir(root_path)
 
 #%%## parameters 
 runs = ['_01'] # ['_01','_02'] for the adults and ['_01'] for the infants
-time = '_t3' # first time (6 mo) '_t1' or second time (12 mo) '_t2' or third time (14mo) '_t3' coming back, or 0 for cbs
+time = '_t2' # first time (6 mo) '_t1' or second time (12 mo) '_t2' or third time (14mo) '_t3' coming back, or 0 for cbs
 direction = "ba_to_pa"
 do_cabr = False # True: use the cABR filter, cov and epoch setting; False: use the MMR filter, cov and epoch setting
 st_correlation = 0.9 # 0.98 for adults and 0.9 for infants
 int_order = 6 # 8 for adults and 6 for infants
-lp = 50 
+lp = 200 
+hp = 80
 subjects = []
 
 for file in os.listdir():
     if file.startswith('sld_136'): # cbs_b for the infants, sld for SLD infants
         subjects.append(file)
-subjects = ['sld_153','sld_154']
+subjects = ['sld_157']
 
 #%%###### do the jobs
 for s in subjects:
@@ -426,10 +428,10 @@ for s in subjects:
             print ('Doing ECG/EOG projection...')
             [raw,raw_erm] = do_projection(s,run,time)
         print ('Doing filtering...')
-        raw_filt = do_filtering(raw,lp, do_cabr)
-        raw_erm_filt = do_filtering(raw_erm,lp, do_cabr)
+        raw_filt = do_filtering(raw,lp, hp, do_cabr)
+        raw_erm_filt = do_filtering(raw_erm,lp, hp, do_cabr)
         print ('Calculate cov...')
-        do_cov(s,raw_erm_filt,time,do_cabr)
+        do_cov(s,raw_erm_filt,time,do_cabr,hp,lp)
         print ('Doing epoch...')
         if do_cabr == True:
             do_epoch_cabr(raw_filt, s, run, time)
