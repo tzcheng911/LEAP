f149c1b7543c2fae7f68c6a94e47839fe8d5e41d
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2025-12-26 14:52:42 -0800

    Update preprocessing_CBS_A.py

--- preprocessing_CBS_A.py
+++ preprocessing_CBS_A.py
@@ -313,6 +313,7 @@ def do_epoch_cabr(data, subject, run, n_trials,hp,lp):
 
 def do_epoch_cabr_eeg(data, subject, run, n_trials):  
     ###### Read the event files (generated from evtag.py) 
+    random.seed(15)
     root_path = os.getcwd()
     cabr_events = mne.read_events(root_path + '/' + subject + '/events/' + subject + run + '_events_cabr-eve.fif')
     file_out = root_path + '/' + subject + '/eeg/' + subject + run 
