99652d808fd117b5c63f836256c9c00a80d2cc3f
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2026-03-19 12:39:07 -0700

    Update src_group_PCFFR_brainstem.py

--- src_group_PCFFR_brainstem.py
+++ src_group_PCFFR_brainstem.py
@@ -9,36 +9,18 @@ Keep the components that include peaks between 90-100 Hz.
 """
 #%%####################################### Import library  
 import mne
-from mne.decoding import UnsupervisedSpatialFilter
-from mne.preprocessing import ICA
-from mne.minimum_norm import apply_inverse_epochs, read_inverse_operator
-from mne.preprocessing import Xdawn
 import matplotlib.pyplot as plt 
 import numpy as np
 import os
-from scipy import stats,signal
-from numpy import dot
-from numpy.linalg import norm
-from scipy.stats import pearsonr
-import scipy as sp
-import os
-import pandas as pd
-import scipy.stats as stats
-from scipy.io import wavfile
-import time
-
-from sklearn.decomposition import PCA, FastICA
-import random
 
 def select_PC(data,sfreq,fmin,fmax,lb,hb,n_top):
     X = data.transpose()
-    
     pca = PCA()
     pca.fit(X) 
     pca_data = pca.fit_transform(X) 
     
     psds, freqs = mne.time_frequency.psd_array_welch(
-        pca_data.transpose()[:,100:],sfreq, # could replace with label time series
+        pca_data.transpose()[:,100:],sfreq, # could replace with label time series # hard coded 100 cuz it's time 0 based on my preprocessing pipeline
         n_fft=len(pca_data.transpose()[0,100:]), # the higher the better freq resolution
         n_overlap=0,
         n_per_seg=None,
@@ -52,13 +34,27 @@ def select_PC(data,sfreq,fmin,fmax,lb,hb,n_top):
     explained_variance_ratio = pca.explained_variance_ratio_[ind_components]
     
     plt.figure()
-    plt.plot(freqs,psds.transpose())
-    # plt.plot(freqs,psds[ind_components,:].transpose(),color='red',linestyle='dashed')
+    plt.plot(freqs,psds[ind_components,:].transpose(),color='red',linestyle='dashed')
+    print('variance explained: ' + str(explained_variance_ratio))
     
+    ## keep only top 3 PC's data
     Xhat = np.dot(pca.transform(X)[:,ind_components], pca.components_[ind_components,:])
     Xhat += np.mean(X, axis=0) 
     Xhat = Xhat.transpose()    
     return pca_data.transpose(),ind_components,explained_variance_ratio,Xhat
+
+def do_foward(s):
+    root_path='/media/tzcheng/storage/Brainstem/'
+    subjects_dir = '/media/tzcheng/storage2/subjects/'
+
+    file_in = root_path + s + '/sss_fif/' 
+    raw_file = mne.io.read_raw_fif(file_in  + s + '_p10_01_otp_raw_sss.fif') # get the info for object with information about the sensors and methods of measurement
+    trans=mne.read_trans(file_in + s + '-trans.fif')
+    src=mne.read_source_spaces(subjects_dir + s + '_zoe/bem/' + s + '_zoe-vol-5-src.fif')
+    bem=mne.read_bem_solution(subjects_dir +  s + '_zoe/bem/' + s + '_zoe-5120-5120-5120-bem-sol.fif')
+    fwd=mne.make_forward_solution(raw_file.info,trans,src,bem,meg=True,eeg=False)
+    mne.write_forward_solution(file_in + s +'-fwd.fif',fwd,overwrite=True)
+    return fwd, src
 
 def do_inverse_FFR(s,evokeds_inv,condition,run,morph,n_top,hp,lp):
     root_path='/media/tzcheng/storage/Brainstem/'
@@ -66,9 +62,8 @@ def do_inverse_FFR(s,evokeds_inv,condition,run,morph,n_top,hp,lp):
 
     file_in = root_path + s + '/sss_fif/' + s
     fwd = mne.read_forward_solution(file_in + '-fwd.fif')
-    cov = mne.read_cov(file_in + run + '_erm_otp_raw_sss_proj_f80200_ffr-cov.fif')
-    epoch = mne.read_epochs(file_in + condition + run + '_otp_raw_sss_proj_f80200_ffr_e.fif')
-    inverse_operator = mne.minimum_norm.make_inverse_operator(epoch.info, fwd, cov,loose=1,depth=0.8)
+    cov = mne.read_cov(file_in + run + '_erm_otp_raw_sss_proj_f' + str(hp) + str(lp) + '_ffr-cov.fif')
+    inverse_operator = mne.minimum_norm.make_inverse_operator(evokeds_inv.info, fwd, cov,loose=1,depth=0.8)
     evokeds_inv_stc = mne.minimum_norm.apply_inverse((evokeds_inv), inverse_operator, pick_ori = None)
 
     if morph == True:
@@ -104,31 +99,37 @@ def group_stc(subj,condition,run,n_top,hp,lp):
     stc_data = stc.data
     stc_roi_data = stc_roi.data
     return stc_data,stc_roi_data
+
 #%%#######################################'
+do_PCA = True
+morph = True
+
 root_path='/media/tzcheng/storage/Brainstem/'
 os.chdir(root_path)
-## Parameters
-fmin = 50
-fmax = 150
-sfreq = 5000
-lb = 90
-hb = 100
-n_top = 3 # change to 10
-n_trial = 200
-lp = 200 # try 200 (suggested by Nike) or 450 (from Coffey paper)
-hp = 80
-runs = ['_01'] # add the ['_01','_02'] for all runs, note that brainstem_107 only has run1 for p10
-conditions = ['_p10','_n40']
-group = []
-group_roi = []
-
-morph = True
 
 subjects = [] 
 for file in os.listdir():
     if file.startswith('brainstem_'):
         subjects.append(file)
+print(subjects)
 
+## preproc parameters
+n_top = 0 # 3 or 10 or 0: indicate no PCA was done
+n_trial = 200
+lp = 200 # try 200 (suggested by Nike) or 450 (from Coffey paper)
+hp = 80
+runs = ['_01'] # only run 01 for now, add the ['_01','_02'] for all runs, note that brainstem_107 only has run1 for p10
+conditions = ['_p10','_n40']
+
+## PCA parameters
+fmin = 50
+fmax = 150
+sfreq = 5000
+lb = 90
+hb = 100
+
+group = []
+group_roi = []
 group_sensor = np.empty([len(subjects),len(conditions),len(runs),306,1101])
 group_morph = np.empty([len(subjects),len(conditions),len(runs),14629,1101])
 group_roi = np.empty([len(subjects),len(conditions),len(runs),114,1101])
@@ -137,24 +138,29 @@ group_pc_info = np.empty([len(subjects),len(conditions),len(runs),n_top,2]) # La
 
 for ns,s in enumerate(subjects):
     print(s)
+    do_foward(s)
     for ncondition,condition in enumerate(conditions):
         for nrun,run in enumerate(runs):
             file_in = root_path + s + '/sss_fif/' + s + condition + run
-            evokeds = mne.read_evokeds(file_in + '_otp_raw_sss_proj_f' + str(hp) + str(lp) + '_evoked_ffr.fif')[0]
-            data = evokeds.get_data()
-            pca_data,ind_components,explained_variance_ratio,data_topPC = select_PC(data,sfreq,fmin,fmax,lb,hb,n_top)
-            evokeds.data = data_topPC
-            group_sensor[ns,ncondition,nrun,:len(data_topPC),:] = data_topPC # somehow brainstem_113 subject only has 305 channels
-            group_pca[ns,ncondition,nrun,:len(data_topPC),:] = pca_data # somehow brainstem_113 subject only has 305 channels
-            group_pc_info[ns,ncondition,nrun,:,0] = ind_components
-            group_pc_info[ns,ncondition,nrun,:,1] = explained_variance_ratio
+            evokeds = mne.read_evokeds(file_in + '_otp_raw_sss_proj_f' + str(hp) + str(lp) + '_ntrial' + str(n_trial) + '_evoked_ffr.fif')[0]
+            if do_PCA:
+                print('Run src on PCA-reduced signals')
+                data = evokeds.get_data()
+                pca_data,ind_components,explained_variance_ratio,data_topPC = select_PC(data,sfreq,fmin,fmax,lb,hb,n_top)
+                evokeds.data = data_topPC
+                group_sensor[ns,ncondition,nrun,:len(data_topPC),:] = data_topPC # somehow brainstem_113 subject only has 305 channels
+                group_pca[ns,ncondition,nrun,:len(data_topPC),:] = pca_data # somehow brainstem_113 subject only has 305 channels
+                group_pc_info[ns,ncondition,nrun,:,0] = ind_components
+                group_pc_info[ns,ncondition,nrun,:,1] = explained_variance_ratio
+            else:
+                print('Run src on non-PCA signals')
             do_inverse_FFR(s,evokeds,condition,run,morph,n_top,hp,lp)
             group_morph[ns,ncondition,nrun,:,:],group_roi[ns,ncondition,nrun,:,:] = group_stc(s,condition,run,n_top,hp,lp)
 
 for ncondition,condition in enumerate(conditions):
     for nrun,run in enumerate(runs):
-        np.save(root_path + '/MEG/FFR/group_pcffr' + str(hp) + str(lp) + '_' + str(n_top) + condition + run + '_sensor.npy',group_sensor[:,ncondition,nrun,:,:])
-        np.save(root_path + '/MEG/FFR/group_pcffr' + str(hp) + str(lp) + '_' + str(n_top) + condition + run + '_' + '_morph.npy',group_morph[:,ncondition,nrun,:,:])
-        np.save(root_path + '/MEG/FFR/group_pcffr' + str(hp) + str(lp) + '_' + str(n_top) + condition + run + '_roi.npy',group_roi[:,ncondition,nrun,:,:])
-        np.save(root_path + '/MEG/FFR/group_f' + str(hp) + str(lp) + '_top_' + str(n_top) + condition + run + 'pc_data.npy',group_pca[:,ncondition,nrun,:,:])
-        np.save(root_path + '/MEG/FFR/group_f' + str(hp) + str(lp) + '_top_' + str(n_top) + condition + run +'pc_info.npy',group_pc_info[:,ncondition,nrun,:,:])
+        np.save(root_path + '/MEG/FFR/group_pcffr' + str(hp) + str(lp) + '_ntrial' + str(n_trial) + '_' + str(n_top) + condition + run + '_sensor.npy',group_sensor[:,ncondition,nrun,:,:])
+        np.save(root_path + '/MEG/FFR/group_pcffr' + str(hp) + str(lp) + '_ntrial' + str(n_trial) + '_' + str(n_top) + condition + run + '_morph.npy',group_morph[:,ncondition,nrun,:,:])
+        np.save(root_path + '/MEG/FFR/group_pcffr' + str(hp) + str(lp) + '_ntrial' + str(n_trial) + '_' + str(n_top) + condition + run + '_roi.npy',group_roi[:,ncondition,nrun,:,:])
+        np.save(root_path + '/MEG/FFR/group_f' + str(hp) + str(lp) + '_ntrial' + str(n_trial) + '_top_' + str(n_top) + condition + run + 'pc_data.npy',group_pca[:,ncondition,nrun,:,:])
+        np.save(root_path + '/MEG/FFR/group_f' + str(hp) + str(lp) + '_ntrial' + str(n_trial) + '_top_' + str(n_top) + condition + run +'pc_info.npy',group_pc_info[:,ncondition,nrun,:,:])
